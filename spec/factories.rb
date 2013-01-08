457fdbd232baa650ed47da40525b963806268882
Chikashige Hayashi <c.hayashi@yahoo.com>
2013-01-08 00:18:08 -0800

    before 7.2.2

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name                   "Simple Rfp"
+    email                  "flan@lauchpad-usa.com"
+    password               "foobar"
+    password_confirmation  "foobar"
+  end
+end
